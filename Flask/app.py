70e386a462654420b43822d98e14bc6762ec5b32
Cosmonautinspace <112541238+cosmonautinspace@users.noreply.github.com>
2024-03-18 17:10:52 +0530

    Add files via upload

---
+++ Flask/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import tensorflow as tf
+
+# A fixed vocabularly of ASCII chars that occur in the works of Shakespeare and Dickens:
+vocab = list('dhlptx@DHLPTX $(,048cgkoswCGKOSW[_#\'/37;?bfjnrvzBFJNRVZ"&*.26:\naeimquyAEIMQUY]!%)-159\r')
+
+# Creating a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+#tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+#model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+model = tf.keras.models.load_model("/content/drive/MyDrive/MajorProject/shake.h5", compile=False)
+
+def generate_text(model, start_string):
+  # From https://www.tensorflow.org/tutorials/sequences/text_generation
+  num_generate = 200
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+  text_generated = []
+  temperature = 1.0
+
+  model.reset_states()
+  for i in range(num_generate):
+    predictions = model(input_eval)
+    predictions = tf.squeeze(predictions, 0)
+    predictions = predictions / temperature
+    predicted_id = tf.random.categorical(
+        predictions, num_samples=1)[-1, 0].numpy()
+    input_eval = tf.expand_dims([predicted_id], 0)
+    text_generated.append(idx2char[predicted_id])
+
+  return (''.join(text_generated))
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return generate_text(input)
+
+
+
+if __name__ == '__main__':
+    app.run()
